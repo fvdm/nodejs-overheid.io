76a00046404ec4bc0582d01f667ebd0e47a0ca70
Franklin van de Meent <fr@nkl.in>
2015-07-07 19:45:28 +0200

    Let the Test be with you

---
+++ test.js
@@ -0,0 +1,98 @@
+/*
+Name:         overheid.io - test.js
+Description:  Access overheid.io endpoints like OpenKvK, RDW and BAG with node.js
+Author:       Franklin van de Meent (https://frankl.in)
+Source code:  https://github.com/fvdm/nodejs-overheid.io
+Feedback:     https://github.com/fvdm/nodejs-overheid.io/issues
+License:      Unlicense (Public Domain) - see LICENSE file
+              (https://github.com/fvdm/nodejs-overheid.io/raw/master/LICENSE)
+*/
+
+// Setup
+var app = require ('./') (
+  process.env.OVIO_APIKEY || null,
+  'rdw',
+  {
+    timeout: process.env.OVIO_TIMEOUT || 5000
+  }
+);
+
+
+// handle exits
+var errors = 0;
+process.on ('exit', function () {
+  if (errors === 0) {
+    console.log ('\n\033[1mDONE, no errors.\033[0m\n');
+    process.exit (0);
+  } else {
+    console.log ('\n\033[1mFAIL, '+ errors +' error'+ (errors > 1 ? 's' : '') +' occurred!\033[0m\n');
+    process.exit (1);
+  }
+});
+
+// prevent errors from killing the process
+process.on ('uncaughtException', function (err) {
+  console.log ();
+  console.error (err.stack);
+  console.trace ();
+  console.log ();
+  errors++;
+});
+
+// Queue to prevent flooding
+var queue = [];
+var next = 0;
+
+function doNext () {
+  next++;
+  if (queue [next]) {
+    queue [next] ();
+  }
+}
+
+// doTest( passErr, 'methods', [
+//   ['feeds', typeof feeds === 'object']
+// ])
+function doTest (err, label, tests) {
+  if (err instanceof Error) {
+    console.error ('\033[1m\033[31mERROR\033[0m - '+ label +'\n');
+    console.dir (err, { depth: null, colors: true });
+    console.log ();
+    console.error (err.stack);
+    console.log ();
+    errors++;
+  } else {
+    var testErrors = [];
+    for (var i = 0; i < tests.length; i++) {
+      if (tests [i] [1] !== true) {
+        testErrors.push (tests [i] [0]);
+        errors++;
+      }
+    }
+
+    if(testErrors.length === 0) {
+      console.log ('\033[1m\033[32mgood\033[0m - '+ label);
+    } else {
+      console.error ('\033[1m\033[31mFAIL\033[0m - '+ label +' ('+ testErrors.join (', ') +')');
+    }
+  }
+
+  doNext ();
+}
+
+
+queue.push (function () {
+  app ('4-TFL-24', { fields: ['eerstekleur'] }, function (err, data) {
+    doTest (err, 'item', [
+      ['data', data && data != null],
+      ['type', data instanceof Object],
+      ['property', data && data.kenteken === '4-TFL-24'],
+      ['field', data && typeof data.eerstekleur === 'string']
+    ]);
+  })
+});
+
+
+// Start the tests
+console.log ('Running tests...\n');
+queue [0] ();
